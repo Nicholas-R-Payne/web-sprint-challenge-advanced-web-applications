cae90c6bd0604fa99a32cf57c04b30296a04c324
DarkedgeTH <darkedgeth@gmail.com>
2022-04-01 09:41:59 -0400

    got Logout button working

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -29,6 +29,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    window.localStorage.removeItem('token')
+    navigate('/')
+    // STILL NEEDS MESSAGE
   }
 
   const login = ({ username, password }) => {
